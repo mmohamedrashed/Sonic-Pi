2ece6caa5c61cd6bdf06650d12d15bfef87226cd
mmohamedrashed <mohamed_rashed11@hotmail.com>
2021-06-21 01:04:19 +0400

    Add files via upload

---
+++ Code_1.rb
@@ -0,0 +1,21 @@
+# First Code: play, delay, looping
+# Second: We add synth
+# use_synth : select a tune
+2. times do
+  play 62
+  sleep 0.45
+  play 63
+  sleep 0.45
+  play 65
+  sleep 0.45
+  play 62
+  sleep 0.45
+end
+2. times do
+  play 64
+  sleep 0.45
+  play 65
+  sleep 0.45
+  play 67
+  sleep 0.8
+end
---
+++ Code_2_(samples).rb
@@ -0,0 +1,17 @@
+# Code 2: Sample Test
+sample :ambi_lunar_land
+sleep 1
+play 48
+sleep 0.5
+play 36
+sample :loop_mika
+sleep 1
+play 36
+
+2. times do
+  play 55
+  sleep 2.5
+  play 60
+  sleep 0.5
+end
+
